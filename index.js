6827936383787493a91ef3bca65f97e4d2c865db
naman277 <153117899+naman277@users.noreply.github.com>
2026-02-23 16:40:04 +0530

    test changes

---
+++ index.js
@@ -0,0 +1,13 @@
+async function movieList() {
+    try{
+        const movies = await fetch('https://8tlaxypk7l.execute-api.us-east-1.amazonaws.com/api/movies');
+        const movienames = await movies.json();
+        console.log(movienames);
+        console.log(movienames.Year);
+    }
+    catch{
+        console.log('Something went wrong');
+    }
+}
+
+movieList();
